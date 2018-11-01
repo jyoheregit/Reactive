73b6cb5f540346c2cdca9cdac550d3fd7a977570
Joe <jyohere.d@gmail.com>
2018-11-01 20:10:52 +0530

    Reactive Playground - First Commit

---
+++ ReactivePlayground.playground/Contents.swift
@@ -0,0 +1,41 @@
+import RxSwift
+import RxCocoa
+
+// Observable
+var names = Observable.of("Joe", "Chris")
+
+names.subscribe({ name in
+    print(name.element ?? name)
+})
+
+print("-------------")
+
+names.subscribe(onNext: { (name) in
+    print(name)
+}, onError: { (error) in
+    print(error)
+}, onCompleted: {
+    print("completed")
+}) {
+    print("disposed")
+}
+print("-------------")
+
+enum MyError : Error {
+    case NotANumber(String)
+}
+
+var anotherObservable : Observable<Int> = Observable.create { (observer) -> Disposable in
+    
+    observer.onNext(1)
+    observer.onError(MyError.NotANumber("Not a number"))
+    observer.onNext(2)
+    observer.onNext(3)
+    observer.onCompleted()
+    
+    return Disposables.create()
+}
+
+anotherObservable.subscribe {
+    print($0.element ?? $0)
+}
